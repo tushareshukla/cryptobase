44d2f1d729ad306bc11f707fdbf893efd38f8b6d
pashupat <tushareshukla@gmail.com>
2024-04-10 16:57:02 +0530

    op-1

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+const Header = () => {
+  return (
+    <div className='navbar'>
+        <h1>CryptoBase<span style ={{color:'var(--blue)'}}>.</span></h1>
+        <div></div>
+
+    </div>
+  )
+}
+
+export default Header
