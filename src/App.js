d08a05dbe7383086a59d6398c532025895fe9205
pashupat <tushareshukla@gmail.com>
2024-04-17 08:30:17 +0530

    main-compnent-hogya-hai

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import  "./App.css"
 import React from 'react'
 import Header from "./components/Common/Header"
-import Footer from "./components/Common/Footer"
+// import Footer from "./components/Common/Footer"
 import MainComponet from "./components/LandingPage/Main"
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
     <div className="app">
         <Header/>
         <MainComponet/>
-        <Footer/>
+        {/* <Footer/> */}
     </div>
   )
 }
--- src/components/LandingPage/Main/index.js
+++ src/components/LandingPage/Main/index.js
@@ -1,29 +1,65 @@
-import Button from "../../Common/Button"
-import "./styles.css"
-import React from 'react'
-import iphone from "../../../assets/iphone.png"
-import gradient from "../../../assets/gradient.png"
+import Button from "../../Common/Button";
+import "./styles.css";
+import React from "react";
+import iphone from "../../../assets/iphone.png";
+import gradient from "../../../assets/gradient.png";
+import { motion } from "framer-motion";
 
 const MainComponet = () => {
   return (
     <div className="flex-info">
-        <div className="left-component">
- {/* import React from 'react'          
-  */}
-  <h1 className="first-heading">Base for</h1>
-  <h1 className="second-heading">CryptoLovers.</h1>
-  <p className="p-tag">Track crypto from with public api realtime with amazing ui</p>
-  <div className="btn-flex">
-    <Button text={"Dashboard"}/>
-    <Button text={'Share App'} outlined={true}/>   
-  </div>
-        </div>
-        <div className="right-component">
-           <img className="iphone" src={iphone} alt="op"></img>
-           <img className="gradinet" src={gradient} alt ="op1"></img>
-            </div>
+      <div className="left-component">
+        {/* import React from 'react'
+         */}
+        <motion.h1
+          className="first-heading"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Base for
+        </motion.h1>
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="second-heading"
+        >
+          CryptoLovers.
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.75 }}
+          className="p-tag"
+        >
+          Track crypto from with public api realtime with amazing ui
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.75 }}
+          className="btn-flex"
+        >
+          <Button text={"Dashboard"} />
+          <Button text={"Share App"} outlined={true} />
+        </motion.div>
+      </div>
+      <div className="right-component">
+        <motion.img
+          initial={{ y: -10 }}
+          animate={{ y: 10 }}
+          transition={{ 
+            type: "smooth",
+            repeatType:"mirror",
+            repeat: Infinity,
+           
+            duration: 2.5 }}
+         className="iphone" src={iphone} alt="op"></motion.img>
+        <img className="gradinet" src={gradient} alt="op1"></img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainComponet
+export default MainComponet;
