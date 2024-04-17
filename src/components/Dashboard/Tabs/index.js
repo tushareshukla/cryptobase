efd0ef386fbf73cd55500ba0999457c6886e889c
pashupat <tushareshukla@gmail.com>
2024-04-17 18:16:38 +0530

    chlogrid-list-hogya

--- src/components/Dashboard/Tabs/index.js
+++ src/components/Dashboard/Tabs/index.js
@@ -3,26 +3,48 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 export default function LabTabs() {
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState('grid');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+
+
+  const theme =createTheme(
+    {
+      palette: {
+        primary: {
+          main: 'var(--blue)'
+        }
+       
+      }
+    }
+  )
+  const styles ={
+    color: 'var(--white)',
+    width: '100%',
+    fontSize:'1.2rem',
+    fontWeight: '600', 
+    fontFamily: 'inter',
+    textTransform: 'capitalize',
+
+  }
   return (
-    <div>
+    <ThemeProvider theme={theme} >
       <TabContext value={value}>
           <TabList onChange={handleChange} variant="fullWidth">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
+            <Tab label="Grid" value="grid" sx={styles}/>
+            <Tab label="List" value="list" sx={styles} />
          
           </TabList>
-        <TabPanel value="1">Item One</TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="grid">Mapping for Grids</TabPanel>
+        <TabPanel value="list">Mapping for Lists</TabPanel>
        
       </TabContext>
-    </div>
+    </ThemeProvider>
   );
 }
