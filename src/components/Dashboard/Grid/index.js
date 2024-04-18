aabcc20b338b80f00b05a2cc727667eadc7de14c
pashupat <tushareshukla@gmail.com>
2024-04-18 17:38:29 +0530

    css-update-grid-bht-mehnat

--- src/components/Dashboard/Grid/index.js
+++ src/components/Dashboard/Grid/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./styles.css";
+import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
+import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
 const Grid = ({ coin }) => {
   return (
     <div className="grid-container">
       <div className="info-flex">
-        <img src ={coin.image} className="coin-logo" alt={coin.id}/>
+        <img src={coin.image} className="coin-logo" alt={coin.id} />
         <div className="name-col">
-          <p>{coin.symbol}</p>
-          <p>{coin.name}</p>
+          <p className="coin-symbol">{coin.symbol}</p>
+          <p className="coin-name">{coin.name}</p>
         </div>
+      
       </div>
+      {coin.price_change_percentage_24h > 0 ? (
+      <div className="chip-flex">
+          <div className="price-chip">{coin.price_change_percentage_24h.toFixed(2)} %</div>
+          <div className="icon-chip"><TrendingUpRoundedIcon/></div>
+        </div>) : (
+           <div className="chip-flex">
+           <div className="price-chip red-chip">{coin.price_change_percentage_24h.toFixed(2)} %</div>
+           <div className="icon-chip icon-chip-red"><TrendingDownRoundedIcon/></div>
+         </div>
+
+        )}
     </div>
   );
 };
