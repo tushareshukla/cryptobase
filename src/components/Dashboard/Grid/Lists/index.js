691221a5b6dad29181ef5757a7026e4f2c832644
pashupat <tushareshukla@gmail.com>
2024-04-18 09:21:15 +0530

    list-grid -init

---
+++ src/components/Dashboard/Grid/Lists/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./styles.css"
+const Lists = () => {
+  return (
+    <div>Lists</div>
+  )
+}
+
+export default Lists
---
+++ src/components/Dashboard/Grid/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./styles.css"
+const Grid = () => {
+  return (
+    <div>Grid</div>
+  )
+}
+
+export default Grid
